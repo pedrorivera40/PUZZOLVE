f372d1696b3162aec70a7ed8fc924fe7377cacc6
Francisco Diaz <francisco.diaz11@upr.edu>
2018-05-09 22:59:55 -0400

    Added obstacle method

--- onix_pygame_code.py
+++ onix_pygame_code.py
@@ -11,31 +11,32 @@ class Screen():
         # initially the matrix will contain zeros
         # if there is an obstacle the position in the matrix will contain a one
         self.obstacle_matrix = np.matrix(np.zeros((square,square)))
-        
+       
+       # I don't know wtf this is
+        self.w = 70
+        self.x = 0
+        self.y = 0
+        self.screen =  pygame.display.set_mode((self.w*self.square,self.w*self.square))
     def createScreen(self,dimensions):
         pygame.init()
-        w = 70
-        x = 0
-        y = 0
         grid = [[1]*dimensions for n in range(dimensions)]
     
         BackGround = pygame.image.load('Images/grass-pattern.png')
         tree = pygame.image.load('Images/tree_sized.png')
-        screen = pygame.display.set_mode((w*dimensions,w*dimensions))
 
-        screen.fill((255,255,255))
-        screen.blit(BackGround, [0,0])
+        self.screen.fill((255,255,255))
+        self.screen.blit(BackGround, [0,0])
 
         for row in grid:
             for column in row:
-                pygame.draw.rect(screen,(0,0,0),(x,y, w,w),1)
-                x = x+ w
-            y = y + w
-            x = 0
+                pygame.draw.rect(self.screen,(0,0,0),(self.x,self.y, self.w,self.w),1)
+                self.x = self.x+ self.w
+            self.y = self.y + self.w
+            self.x = 0
 
-        screen.blit(tree, [0,0])
-        screen.blit(tree, [w*8,w*5])
-
+        self.screen.blit(tree, [0,0])
+        self.screen.blit(tree, [self.w*8,self.w*5])
+        self.add_obstacle(tree,3,2)
 
         pygame.display.flip()
 
@@ -44,9 +45,12 @@ class Screen():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False 
+    
+    def add_obstacle(self,tree,x,y):
+        self.obstacle_matrix[y-1,x-1] = 1
+        self.screen.blit(image,[self.w*(x-1),self.w*(y-1)])
 
 screen = Screen(10)
 screen.createScreen(10)
-
  
-
+tree = pygame.image.load('Images/tree_sized.png')
